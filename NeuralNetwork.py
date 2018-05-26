282d27d1c92ed5aec10abe88a7397f77a333ddd9
Marcin <marcin919@op.pl>
2018-05-26 23:54:31 +0200

    backpropagate function without test, horizontal instead of certical matrixes -_-

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,12 @@
-from numpy import random, exp, dot, array, power
+from numpy import random, exp, dot, array, power, zeros
+
+
+def sigmoid(x):
+    return 1 / (1 + exp(-x))
+
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
 
 class NeuralNetwork:
@@ -12,44 +20,67 @@ class NeuralNetwork:
         self.first_weights = 2 * random.random((self.hidden_layer_neuron_number, self.input_layer_neuron_number)) - 1
         self.second_weights = 2 * random.random((self.output_layer_neuron_number, self.hidden_layer_neuron_number)) - 1
 
-    @staticmethod
-    def sigmoid(x):
-        return 1 / (1 + exp(-x))
+    def feed_forward(self, inputData):
+        first_result = self.sigmoid(dot(self.first_weights, inputData))
+        result = self.sigmoid(dot(self.second_weights, first_result))
+        return result
 
-    @staticmethod
-    def sigmoid_derivative(x):
-        return x * (1 - x)
+    def backpropagate(self, inputData, outputData):
+        g2 = [zeros(weight.shape) for weight in self.second_weights]
+        g1 = [zeros(weight.shape) for weight in self.first_weights]
 
-    def feed_forward(self, input):
-        first_result = self.sigmoid(dot(self.first_weights, input))
-        return self.sigmoid(dot(self.second_weights, first_result))
+        A1 = inputData
+        Z1 = dot(self.first_weights, A1)
+        A2 = sigmoid(Z1)
+        Z2 = dot(self.second_weights, A2)
+        A3 = sigmoid(Z2)
+
+        d2 = (A3 - outputData) * sigmoid_derivative(Z2)
+        g2 = dot(d2, A2.transpose())
+
+        d1 = dot(self.second_weights.transpose(), d2) * sigmoid_derivative(Z1)
+        g1 = dot(d1, A1.transpose())
+
+        return g1, g2
 
     def train(self):
-        input = array([1]).transpose()
-        output = array([2]).transpose()
+        inputData = array([[1], [0]])
+        outputData = array([[1], [0]])
+        self.init_layers(2, 3, 2)
+        self.init_weights()
 
-        for i in range(10):
-            self.init_layers(1, 1, 1)
-            self.init_weights()
+        self.backpropagate(inputData, outputData)
+        # for i in range(1000000000):
+        #
+        #     result_output = self.feed_forward(inputData)
+        #
+        #     error = output - result_output
+        #     square_error = (power(error, 2)) / 2
+        #     derivative = self.sigmoid_derivative(sum(square_error))
+        #     # if derivative > 0:
+        #     #     self.first_weights -= (derivative * 0.000001)
+        #     #     self.second_weights -= (derivative * 0.000001)
+        #     # elif derivative < 0:
+        #     #     self.first_weights += (derivative * 0.000001)
+        #     #     self.second_weights += (derivative * 0.000001)
+        #     # else:
+        #     #     break
+        #
+        #     if i % 100000 == 0:
+        #         print("result: ", result_output)
+        #         print("error: ", square_error)
+        #         print("weights1: ", self.first_weights)
+        #         print("weights2: ", self.second_weights)
+        #         print("derivative: ", derivative)
+        #         print()
 
-            result_output = self.feed_forward(input)
 
-            error = output - result_output
-            square_error = power(error, 2)
+# output_delta = error * self.sigmoid_derivative(result_output)
+# self.first_weights += dot(first_layer, output_delta)
+# self.second_weights += dot(first_layer, output_delta)
 
-            print("result: ", result_output)
-            print("error: ", square_error)
-            print("weights1: ", self.first_weights)
-            print("weights2: ", self.second_weights)
-            print()
-            print()
-
-            # output_delta = error * self.sigmoid_derivative(result_output)
-            # self.first_weights += dot(first_layer, output_delta)
-            # self.second_weights += dot(first_layer, output_delta)
-
-            # print("Output After Training:")
-            # print(result_output)
+# print("Output After Training:")
+# print(result_output)
 
 
 neural_network = NeuralNetwork()
