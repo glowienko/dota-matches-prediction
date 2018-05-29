eb5fe7e0e2b9a5c20895b5441f751dad3ef90211
Marcin <marcin919@op.pl>
2018-05-29 16:11:25 +0200

    Probably working backpropagation and weights updates, test example on 1, 10, 3 NN

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -21,8 +21,8 @@ class NeuralNetwork:
         self.second_weights = 2 * random.random((self.output_layer_neuron_number, self.hidden_layer_neuron_number)) - 1
 
     def feed_forward(self, inputData):
-        first_result = self.sigmoid(dot(self.first_weights, inputData))
-        result = self.sigmoid(dot(self.second_weights, first_result))
+        first_result = sigmoid(dot(self.first_weights, inputData))
+        result = sigmoid(dot(self.second_weights, first_result))
         return result
 
     def backpropagate(self, inputData, outputData):
@@ -43,54 +43,60 @@ class NeuralNetwork:
 
         return g1, g2
 
+    def updateWeights(self, gradient1, gradient2, eta):
+        for i in range(len(self.first_weights)):
+            for x in range(len(self.first_weights[i])):
+                self.first_weights[i, x] = self.first_weights[i, x] - eta * gradient1[i, x]
+
+        for i in range(len(self.second_weights)):
+            for x in range(len(self.second_weights[i])):
+                self.second_weights[i, x] = self.second_weights[i, x] - eta * gradient2[i, x]
+
     def train(self):
-        inputData = array([[1], [0]])
-        outputData = array([[1], [0]])
-        self.init_layers(2, 3, 2)
-        self.init_weights()
 
-        self.backpropagate(inputData, outputData)
-        # for i in range(1000000000):
-        #
-        #     result_output = self.feed_forward(inputData)
-        #
-        #     error = output - result_output
-        #     square_error = (power(error, 2)) / 2
-        #     derivative = self.sigmoid_derivative(sum(square_error))
-        #     # if derivative > 0:
-        #     #     self.first_weights -= (derivative * 0.000001)
-        #     #     self.second_weights -= (derivative * 0.000001)
-        #     # elif derivative < 0:
-        #     #     self.first_weights += (derivative * 0.000001)
-        #     #     self.second_weights += (derivative * 0.000001)
-        #     # else:
-        #     #     break
-        #
-        #     if i % 100000 == 0:
-        #         print("result: ", result_output)
-        #         print("error: ", square_error)
-        #         print("weights1: ", self.first_weights)
-        #         print("weights2: ", self.second_weights)
-        #         print("derivative: ", derivative)
-        #         print()
+        for i in range(300000):
+            inputData = array([[0]])
+            outputData = array([[1], [0], [0]])
 
 
-# output_delta = error * self.sigmoid_derivative(result_output)
-# self.first_weights += dot(first_layer, output_delta)
-# self.second_weights += dot(first_layer, output_delta)
+            g1, g2 = self.backpropagate(inputData, outputData)
+            self.updateWeights(g1, g2, 0.001)
 
-# print("Output After Training:")
-# print(result_output)
+            inputData = array([[1]])
+            outputData = array([[0], [1], [0]])
+
+            g1, g2 = self.backpropagate(inputData, outputData)
+            self.updateWeights(g1, g2, 0.001)
+
+            inputData = array([[2]])
+            outputData = array([[0], [0], [1]])
+
+            g1, g2 = self.backpropagate(inputData, outputData)
+            self.updateWeights(g1, g2, 0.001)
 
 
 neural_network = NeuralNetwork()
-# neural_network.init_layers(4, 3)
-# neural_network.init_weights()
-#
-# result = neural_network.feed_forward(array([0.1, 0.1, 0.9, 0.9]).transpose())
-# print()
-# print()
-# print("Result: ")
-# print(result)
+
+neural_network.init_layers(1, 10, 3)
+neural_network.init_weights()
+
+result1a = neural_network.feed_forward([[0]])
+result1b = neural_network.feed_forward([[1]])
+result1c = neural_network.feed_forward([[2]])
+print(f'Result1:\n'
+      f'0: {result1a}\n'
+      f'1: {result1b}\n'
+      f'2: {result1c}')
 
 neural_network.train()
+
+result2a = neural_network.feed_forward([[0]])
+result2b = neural_network.feed_forward([[1]])
+result2c = neural_network.feed_forward([[2]])
+print(f'Result2:\n'
+      f'0: {result2a}\n'
+      f'1: {result2b}\n'
+      f'2: {result2c}')
+
+
+
